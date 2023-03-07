fcb564fe84113baf25d60e25ff0f76ad65e18e3b
dvane87 <39720957+dvane87@users.noreply.github.com>
2023-03-07 20:22:51 -0300

    Delete aplicaciones.js

--- aplicaciones.js
+++
@@ -1,8 +0,0 @@
-const toggle = document.querySelector('.toggle')
-const link = document.querySelector('.link')
-
-toggle.addEventListener('click',() => {
-    toggle.classList.toggle('rotate')
-    link.classList.toggle('activo')
-
-})
